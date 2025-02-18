639ff6cc28c2df579ded8b01b48ddd387b43bccf
bdshakhawat <mdshakwat.hossain@gmail.com>
2025-02-18 23:18:29 +0600

    Database connected successfully

--- backend/app.js
+++ backend/app.js
@@ -14,14 +14,5 @@ app.get('/', async (req, res) => {
     res.status(500).send('Database connection failed.');
   }
 });
-// app.get('/books', async (req, res) => {
-//   try {
-//     const [rows] = await pool.query('SELECT * FROM books');
-//     res.json(rows);
-//   } catch (err) {
-//     console.error('Error fetching books:', err);
-//     res.status(500).send('Error fetching books.');
-//   }
-// });
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
-import 'dotenv/config'; // Load environment variables from .env file
-import app from './app.js'; // Import the app instance
+import 'dotenv/config';
+import app from './app.js';
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Book Store app is listening on port ${PORT}.`);
 });
