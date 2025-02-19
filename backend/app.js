3e96cd53ffcc7f1898060a6abed816d4c459a033
bdshakhawat <mdshakwat.hossain@gmail.com>
2025-02-19 15:37:05 +0600

    API end points created

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,45 @@ import pool from './db.js'; // Import the database connection
 
 const app = express();
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Route to create a new book
+app.post("/books", async (req, res) => {
+  try{
+    const{title, author, price} = req.body;
+    // validate the input
+    if(!title || !author || !price){
+      return res.status(400).json({error:"Failed to create book"});
+    }
+    // insert the new book into the database
+    const [result] = await pool.query(
+      'INSERT INTO books (title, author, price) VALUES (?, ?, ?)',
+      [title, author, price]
+    );
+    res.status(201).json({message: "Book created successfully", bookId: result.insertId, title, author, price});
+
+  }
+ catch(err){
+  console.error(err);
+  res.status(500).json({error: "Failed to create book"});
+}
+});
+
+
+// Route to get all books
+app.get("/books", async (req, res) => {
+  try{
+    const [rows] = await pool.query('SELECT * FROM books');
+    res.status(200).json(rows);
+  }
+  catch(err){
+    console.error("Error Fetching book: ",  err);
+    res.status(500).json({error: "Failed to get books"});
+  }
+});
+
+
 // Test database connection
 app.get('/', async (req, res) => {
   try {
