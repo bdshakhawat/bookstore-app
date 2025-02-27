44df4d76e9eb7762d340a06924f2a71d47394df6
bdshakhawat <mdshakwat.hossain@gmail.com>
2025-02-27 21:48:22 +0600

    backend authentication

--- backend/middleware/clerkAuth.js
+++ backend/middleware/clerkAuth.js
@@ -1,3 +1,10 @@
+// import { Clerk } from '@clerk/clerk-sdk-node';
+// export const requireAuth = ClerkExpressWithAuth({});
+
+// app.get("/books", requireAuth, async (req, res) => {
+//   console.log(req.auth); // <-- Add this
+// });
+
 import { ClerkExpressWithAuth } from "@clerk/clerk-sdk-node";
 
 export const requireAuth = ClerkExpressWithAuth({});
--- frontend/middleware.js
+++ frontend/middleware.js
@@ -9,3 +9,4 @@ export default authMiddleware({
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
